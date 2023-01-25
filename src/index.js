011d0c67c56c727deedbc1a446195f00b7cdc231
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-25 16:20:06 +0300

    fix: fixed bugs

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@
 
 module.exports = function towelSort (matrix) {
   let result;
-  if (matrix[1].length == 0) {
-      let arr1 = [];
-      matrix[1].push(arr1);
-      return result = matrix.flat();
-  } else {
-      let arr1 = matrix[1];
-      let reverseArr1 = arr1.reverse();
-      matrix[1] = reverseArr1;
-      
-      return result = matrix.flat();
-  }
+    if (matrix[1].length == 0) {
+        let empty = [];
+        matrix[1].push(empty);
+        return result = matrix.flat();
+    } else if (matrix.length == 0) {
+        return [];
+    } else {
+        let arr1 = matrix[1];
+        let reverseArr1 = arr1.reverse();
+        matrix[1] = reverseArr1;
+        
+        return result = matrix.flat();
+    }
 }

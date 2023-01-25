6dfc4a00e03d087f6f41e0526a7e6b2697e84466
Anton Leshkovich <antonleshkovich@mail.ru>
2023-01-25 14:32:08 +0300

    fix: fixed bugs

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@
 
 module.exports = function towelSort (matrix) {
   let arr = matrix.flat();
-    function sortArray (a, b) {
-        return a - b;
-    }
+    // function sortArray (a, b) {
+    //     return a - b;
+    // }
     
-    return arr.sort(sortArray)
+    return arr;
 }
